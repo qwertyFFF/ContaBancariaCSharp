ecd24134bba417f65ad0bbecbe3b2f5eca94e5a1
Alessandro Freitas <37306773+qwertyFFF@users.noreply.github.com>
2018-06-15 17:51:50 -0300

    Add files via upload

---
+++ ContaBancaria/ContaBancaria/Conta.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace ContaBancaria
+{
+    class Conta
+    {
+        public int numero { get; private set; }
+        public string titular { get; set; }
+        public double saldo { get; private set; }
+
+        public Conta(int numero, string titular, double saldo)
+        {
+            this.numero = numero;
+            this.titular = titular;
+            this.saldo = saldo;
+        }
+
+        //Método para depositar na conta uma quantia
+        public void Depositar(double quantia)
+        {
+            saldo = saldo + quantia;
+        }
+
+        //Método para sacar uma quantia da conta + taxa (5 reais por saque)
+        public void Sacar(double quantia)
+        {
+            saldo = (saldo - quantia) - 5.00;
+        }
+
+        //Método para informar os dados atuais da conta
+        public override string ToString()
+        {
+            return "Número da conta: " + numero + ", Titular da conta: " + titular + ", Saldo: $ " + saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variaveis
+            char escolha;
+            double valorInicial = 0.00;
+            int operacao = 0;
+            int numConta;
+            string nomeTit;
+            double dep, saq;
+
+            
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("---BEM VINDO AO BANCO TESTE---");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("1 - Criar conta\n2 - Depositar\n3 - Sacar\n4 - Informações da conta\n0 - Sair");
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("Qual o operação deseja realizar: ");
+                operacao = int.Parse(Console.ReadLine());
+
+
+
+                switch (operacao)
+                {
+                    case 1:
+                        //Criação da conta
+                        Conta conta1;
+
+                        //Informar os dados da conta para ser criada
+                        Console.Write("Digite o número da conta: ");
+                        numConta = int.Parse(Console.ReadLine());
+                        Console.Write("Digite o nome do titular da conta: ");
+                        nomeTit = Console.ReadLine();
+
+                        //Informar se o usuário quer ou não depositar um valor inicialmente
+                        Console.Write("Haverá depósito inicial (s/n)? ");
+                        escolha = char.Parse(Console.ReadLine());
+                        Console.WriteLine();
+
+                            //Valida se o usuário quer depositar o valor inicial na criação da conta
+                            if (escolha == 's')
+                            {
+                                Console.Write("Digite o valor do depósito inicial: ");
+                                valorInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                                Console.WriteLine();
+                            }
+
+                        //Mostra na tela os dados da conta
+                        Console.WriteLine(">>>>>>>>>Conta criada<<<<<<<<<");
+                        conta1 = new Conta(numConta, nomeTit, valorInicial);
+                        Console.WriteLine(conta1);
+
+                        Console.ReadLine();
+                        break;
+
+                    case 2:
+                        //Informar um valor para depositar na conta
+                        Console.WriteLine();
+                        Console.Write("Digite um valor para depósito: ");
+                        dep = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        conta1.Depositar(dep);
+
+                        //Atualiza a conta conforme o depósito na conta
+                        Console.WriteLine();
+                        Console.WriteLine(">>>>>>>>>Conta atualizada<<<<<<<<<");
+                        Console.WriteLine(conta1);
+
+                        Console.ReadLine();
+                        break;
+
+                    case 3:
+                        //Informar um valor para sacar da conta
+                        Console.WriteLine();
+                        Console.Write("Digite um valor saque: ");
+                        saq = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        conta1.Sacar(saq);
+
+                        //Atualiza a conta conforme o saque da conta
+                        Console.WriteLine();
+                        Console.WriteLine(">>>>>>>>>Conta atualizada<<<<<<<<<");
+                        Console.WriteLine(conta1);
+                        Console.ReadLine();
+                        break;
+                }
+            }
+            while (operacao != 0);
+
+            //Informar um valor para sacar da conta
+            //Console.WriteLine();
+            //Console.Write("Digite um valor saque: ");
+            //saq = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //conta1.Sacar(saq);
+
+            //Atualiza a conta conforme o saque da conta
+            //Console.WriteLine();
+            //Console.WriteLine(">>>>>>>>>Conta atualizada<<<<<<<<<");
+            //Console.WriteLine(conta1);
+
+            //Aguarda o usuário finalizar o acesso a conta
+            //Console.ReadLine();
+        }
+    }
+}
